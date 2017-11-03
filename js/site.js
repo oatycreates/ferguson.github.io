4b218c8a59e1bbe086361bdd8303fa40a10d108d
Patrick Ferguson <patrick@ferguson.cloud>
2017-11-04 00:51:09 +1100

    Add contact form using formspree.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,21 @@
 (function () {
-  // Site code here
+  var contactFormSelector = "#contact-form";
+  var contactFormNameSelector = "#contact-input-name";
+  var contactFormEmailSubjectSelector = "#contact-email-subject";
+
+  var onContactFormSubmit = function () {
+    // Include the user's name in the contact form subject to assist email threading
+    var $nameInput = $(contactFormNameSelector);
+    var $emailSubject = $(contactFormEmailSubjectSelector);
+
+    var name = $nameInput.val();
+    $emailSubject.val("New submission from patrickferguson.cloud - " + name);
+  }
+
+  var initSiteScript = function() {
+    var $contactForm = $(contactFormSelector);
+    $contactForm.on('submit', onContactFormSubmit);
+  };
+
+  $(initSiteScript);
 }());
